22a6b8b15a7d70ee9c7ddb8e630b36eb130cbfe3
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-09-30 12:18:14 +0700

    Update __init__.py

--- JisooX/__init__.py
+++ JisooX/__init__.py
@@ -130,23 +130,23 @@ else:
     OWNER_USERNAME = Config.OWNER_USERNAME
 
     try:
-        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or []}
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or []}
+        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or []) 
+        DEV_USERS = set(int(x) for x in Config.DEV_USERS or []) 
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or []}
+        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or []) 
     except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        SPAMMERS = set(int(x) for x in Config.SPAMMERS or []}
+        SPAMMERS = set(int(x) for x in Config.SPAMMERS or []) 
     except ValueError:
         raise Exception("Your spammers users list does not contain valid integers.")
 
     try:
-        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or []}
+        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or []) 
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
