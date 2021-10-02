2c3da285245eda5f456e276dab75655ded9e4cd0
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-10-03 00:57:49 +0700

    Update connection.py

--- JisooX/modules/connection.py
+++ JisooX/modules/connection.py
@@ -284,10 +284,10 @@ def connect_button(bot: Bot, update: Update):
         connect_chat(bot, update, [])
 
 __help__ = """
- - /connect: connect a chat (Can be done in a group by /connect or /connect <chat id> in PM)
- - /connection: list connected chats
- - /disconnect: disconnect from a chat
- - /helpconnect: list available commands that can be done remotely
+ ✘ /connect : connect a chat (Can be done in a group by /connect or /connect <chat id> in PM)
+ ✘ /connection : list connected chats! 
+ ✘ /disconnect : disconnect from a chat! 
+ ✘ /helpconnect : list available commands that can be done remotely! 
 
 *Admin only:*
  - /allowconnect <yes/no>: allow a user to connect to a chat
